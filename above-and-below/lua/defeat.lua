b82ee24beaa64bcec1ceddf38628442cb3ab9431
Sventimir <svantimir@gmail.com>
2024-07-20 17:06:08 +0200

    Add defeat-on-time condition.

--- above-and-below/lua/defeat.lua
+++ above-and-below/lua/defeat.lua
@@ -3,9 +3,12 @@ local messages = {
   "…szkoda zdzierac nóg.",
   "Raczej gardła, łośku. To koniec."
 }
+local first_speaker = wml.variables_proxy.unit or { side = 1 }
+
 aab_run_dialog(
   wesnoth.units.find({ side = player_sides, canrecruit = true }),
-  messages
+  messages,
+  first_speaker.side
 )
 
 wesnoth.wml_actions.endlevel({
