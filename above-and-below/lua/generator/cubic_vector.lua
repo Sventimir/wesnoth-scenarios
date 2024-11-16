65af041c6f1a8d41688f4467178c98c085f1bb08
Sventimir <svantimir@gmail.com>
2024-11-16 16:25:05 +0100

    Improvements and bugfixes for vector library

--- above-and-below/lua/generator/cubic_vector.lua
+++ above-and-below/lua/generator/cubic_vector.lua
@@ -28,19 +28,20 @@ function Vec:__eq(other)
 end
 
 function Vec:__tostring()
-  return string.format("(%d, %d, %d)", self:sw(), self.s, self.se)
+  return string.format("[%d, %d, %d]", self:sw(), self.s, self.se)
 end
 
 function Vec:scale(f)
-  return Vec.new(self.s * f, self.se * f)
+  return Vec.new(mathx.floor(self.s * f), mathx.floor(self.se * f))
 end
 
 function Vec:translate(x, y)
-  return x + self.se, y + self.s + math.floor(self.se / 2)
+  local round = (x % 2 == 0) and mathx.ceil or mathx.floor
+  return x + self.se, y + self.s + round(self.se / 2)
 end
 
 function Vec:length()
-  return math.max(math.abs(self.s), math.abs(self.se), math.abs(self.s + self.se))
+  return mathx.max(mathx.abs(self.s), mathx.abs(self.se), mathx.abs(self.s + self.se))
 end
 
 -- We've got two possibilities. Either self.s and self.se have the same sign,
@@ -59,27 +60,48 @@ function Vec.equidistant(radius)
     end
   end
   return chain(
-    with_nats(function(n) return Vec.new(n, radius - n) end), -- (0, 1)
-    with_nats(function(n) return Vec.new(-n, -radius + n) end), -- (0, -1)
-    with_nats(function(n) return Vec.new(radius, -n) end), -- (1, 0)
     with_nats(function(n) return Vec.new(-radius, n) end), -- (-1, 0)
     with_nats(function(n) return Vec.new(-n - 1, radius) end), -- (-1, 1)
-    with_nats(function(n) return Vec.new(n + 1, -radius) end) -- (1, -1)
+    with_nats(function(n) return Vec.new(n, radius - n) end), -- (0, 1)
+    with_nats(function(n) return Vec.new(radius, -n) end), -- (1, 0)
+    with_nats(function(n) return Vec.new(n + 1, -radius) end), -- (1, -1)
+    with_nats(function(n) return Vec.new(-n, -radius + n) end) -- (0, -1)
   )
 end
 
 Vec.eigenvector = {
-  n = Vec.new(-1, 0),
-  ne = Vec.new(-1, 1),
-  se = Vec.new(0, 1),
-  s = Vec.new(1, 0),
-  sw = Vec.new(1, -1),
-  nw = Vec.new(0, -1)
+  n = Vec.new(-1, 0), -- 1
+  ne = Vec.new(-1, 1), -- 2
+  se = Vec.new(0, 1), -- 5
+  s = Vec.new(1, 0), -- 7
+  sw = Vec.new(1, -1), -- 6
+  nw = Vec.new(0, -1) -- 3
 }
 
+function Vec.eigenvector.id(v)
+  return (v.s + 1) * 3 + v.se + 1
+end
+
+local eigenvector_names = { [0] = "n", "ne", "se", "s", "sw", "nw" }
+
 function Vec.eigenvector.random()
-  local names = { "n", "ne", "se", "s", "sw", "nw" }
-  return Vec.eigenvector[names[mathx.random(1, 6)]]
+  return Vec.eigenvector[eigenvector_names[mathx.random(0, 5)]]
+end
+
+-- Return a triplet of 3 eigenvectors that are not equal and not opposite of each other.
+function Vec.eigenvector.random_triplet()
+  local choice = mathx.random(0, 5)
+  return Vec.eigenvector[eigenvector_names[choice]],
+    Vec.eigenvector[eigenvector_names[(choice + 1) % 6]],
+    Vec.eigenvector[eigenvector_names[(choice + 2) % 6]]
+end
+
+function Vec.eigenvector.clockwise(v)
+  return Vec.new(v.s + v.se, -v.s)
+end
+
+function Vec.eigenvector.counterclockwise(v)
+  return Vec.new(-v.se, v.se + v.s)
 end
 
 return Vec
